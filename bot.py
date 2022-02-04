8f10f40d0118db2ba9757039d2a3a5a8b4fff2d2
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-02-04 07:34:42 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ from config import (
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
     FORCE_SUB_CHANNEL1,
-    FORCE_SUB_GROUP1,
     LOGGER,
     OWNER,
     TG_BOT_TOKEN,
@@ -52,7 +51,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/KayzuSupport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_GROUP:
@@ -68,7 +67,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_GROUP dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/KayzuSupport untuk Bantuan"
                 )
                 sys.exit()
         if FORCE_SUB_CHANNEL1:
@@ -84,23 +83,7 @@ class Bot(Client):
                     f"Silakan periksa kembali var FORCE_SUB_CHANNEL1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Channel Saat Ini: {FORCE_SUB_CHANNEL}"
                 )
                 self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
-                )
-                sys.exit()
-        if FORCE_SUB_GROUP1:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB_GROUP1)
-                self.invitelink4 = link
-            except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning(
-                    "Bot tidak dapat Mengambil link Undangan dari FORCE_SUB_GROUP1!"
-                )
-                self.LOGGER(__name__).warning(
-                    f"Silakan periksa kembali var FORCE_SUB_GROUP1 dan Pastikan Bot anda Admin di Channel dengan izin link invite Pengguna melalui link undangan, Subs Group Saat Ini: {FORCE_SUB_GROUP}"
-                )
-                self.LOGGER(__name__).info(
-                    "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                    "\nBot Berhenti. Gabung Group https://t.me/KayzuSupport untuk Bantuan"
                 )
                 sys.exit()
         try:
@@ -114,13 +97,13 @@ class Bot(Client):
                 f"Pastikan Bot adalah Admin di Channel DataBase, dan Periksa kembali Nilai CHANNEL_ID, Nilai Saat Ini: {CHANNEL_ID}"
             )
             self.LOGGER(__name__).info(
-                "\nBot Berhenti. Gabung Group https://t.me/AyiinXdSupport untuk Bantuan"
+                "\nBot Berhenti. Gabung Group https://t.me/KayzuSupport untuk Bantuan"
             )
             sys.exit()
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/AyiinXdSupport"
+            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan ke https://t.me/Kayzuuuuu"
         )
         self.username = usr_bot_me.username
 

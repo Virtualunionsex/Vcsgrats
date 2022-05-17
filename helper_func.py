744623f19627858e578c841216d932ee41dc25db
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-05-18 02:27:07 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -18,8 +18,6 @@ async def is_subscribed(filter, client, update):
         return True
     if not FORCE_SUB_CHANNEL1:
         return True
-    if not FORCE_SUB_CHANNEL2:
-        return True
     user_id = update.from_user.id
     if user_id in ADMINS:
         return True

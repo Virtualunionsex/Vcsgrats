ee560daff58c895428a226089513a802548ecf95
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-05-29 06:54:27 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -11,12 +11,10 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
-from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1
+from config import ADMINS, FORCE_SUB_CHANNEL
 
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
-        return True
-    if not FORCE_SUB_CHANNEL1:
         return True
     user_id = update.from_user.id
     if user_id in ADMINS:
